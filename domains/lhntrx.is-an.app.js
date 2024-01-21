720c41f72d6ef9fec3206103d545c34058e79c6a
Abram Satria <abramsatria7@gmail.com>
2024-01-21 08:40:21 +0700

    Create lhntrx.is-an.app.js

---
+++ domains/lhntrx.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Portfolio',
+  domain: 'is-an.app',
+  subdomain: 'lhntrx',
+  owner: {
+    email: 'abramsatria7@gmail.com',
+  },
+  record: {
+    CNAME: 'c14099c8-c7cd-4184-9c67-d9824cfd54ed-00-23u2gu3jk3do9.picard.replit.dev',
+  },
+  proxy: false,
+})
